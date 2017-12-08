df70544e93eb5aa48b30acf7769bfaf5286093d1
Kim Gonzales <kimgo589@gmail.com>
2017-12-08 00:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var name = "Joe"
 var height = "74"
 if (name==="Susan") {
   console.log("The name is correct")
-}
-else {
-  console.log("Expected the name to break
+} else {
+  console.log("Expected the name to be
 Susan")
 }
 
